a228f1a438d87b3c682c6884e2a5e2f3d4d744b9
FuryOfD <dharshanng@gmail.com>
2024-03-26 11:28:58 +0200

    ✨ added score count

--- Assignment 2/server.py
+++ Assignment 2/server.py
@@ -14,12 +14,14 @@ class Question:
 def read_questions(filename):
     questions = []
     question = None
+    num_questions = 0
     with open(filename, 'r') as file:
         lines = file.readlines()
         for line in lines:
             if line.startswith('?'):
                 if question:
                     questions.append(question)
+                    num_questions += 1
                 question = Question(line[1:], [], '')
             elif line.startswith('+'):
                 question.correct_answer = line[1:]
@@ -34,8 +36,14 @@ def read_questions(filename):
 def select_question(questions):
     return random.choice(questions)
 
+#global score, total  # Declare score and total as global variables
+score = 0  # Initialize score
+total = 0  # Initialize total
+
 # Function to handle a client's connection
 def handle_client_connection(client_socket, questions):
+    global score, total  # Declare score and total as global variables
+    
     selected_question = select_question(questions)
     question_text = selected_question.question
     answers = selected_question.answers
@@ -55,8 +63,12 @@ def handle_client_connection(client_socket, questions):
     if user_answer in ['A', 'B', 'C', 'D', 'E']:
         if answers[ord(user_answer) - ord('A')] == selected_question.correct_answer:
             client_socket.sendall(b"Correct answer!\n")
+            score += 1
+            total += 1
         else:
             client_socket.sendall(b"Incorrect answer!\n The correct answer is: " + selected_question.correct_answer.encode() + b"\n")
+            total += 1
+            
     else:
         client_socket.sendall(b"Invalid answer. Please enter A, B, C, or D.\n")
     
@@ -66,6 +78,8 @@ def handle_client_connection(client_socket, questions):
     if response == 'y':
         handle_client_connection(client_socket, questions)
     else:
+        #client_socket.sendall(b"Your final score is: " + str(score).encode() +"/" + str(total).encode()+ b"\n")
+        client_socket.sendall(f"Your final score is: {score}/{total}\n".encode())
         client_socket.sendall(b"Thank you for playing! Goodbye.\n")
         client_socket.close()
 
