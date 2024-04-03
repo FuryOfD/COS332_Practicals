4a9435e2947247dc2491ff8846e0649f2b9ff1af
FuryOfD <dharshanng@gmail.com>
2024-04-04 01:23:50 +0200

    Main class added

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try{
+            ServerSocket server = new ServerSocket(8080);
+
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
