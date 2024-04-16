d484f4532d37309cf642221653f81435c0970fe0
Rukie007 <ruan@ufcons.com>
2024-04-16 21:11:44 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
-from urllib.parse import parse_qs
+import socket
 import random
+from urllib.parse import unquote
 
 class Question:
     def __init__(self, question, answers, correct_answer):
@@ -11,14 +11,12 @@ class Question:
 def read_questions(filename):
     questions = []
     question = None
-    num_questions = 0
     with open(filename, 'r') as file:
         lines = file.readlines()
         for line in lines:
             if line.startswith('?'):
                 if question:
                     questions.append(question)
-                    num_questions += 1
                 question = Question(line[1:], [], '')
             elif line.startswith('+'):
                 question.correct_answer = line[1:]
@@ -32,103 +30,136 @@ def read_questions(filename):
 def select_question(questions):
     return random.choice(questions)
 
-html_template = """<!DOCTYPE html>
-<html>
-    <head>
-        <title>COS 332 Assignment 4</title>
-    </head>
-    <body>
-        <h1>Quiz</h1>
-        <h2>{question}</h2>
-        <form method="post" action="/questionpage">
-            {answers}
-            <input type="submit" value="Submit">
-        </form>
-    </body>
-</html>
-"""
-
-html_response = """
-            <!DOCTYPE html>
-            <html>
-                <head>
-                    <title>COS 332 Assignment 4</title>
-                </head>
-                <body>
-                    <h1>Quiz</h1>
-                    <h2>{response}</h2>
-                    <form method = "get" action = "/responsepage">
-                        <input type="submit" value="Next Question">
-                    </form>
-                    
-                    <form method = "get" action = "/endquiz">
-                        <input type="submit" value="End quiz">
-                    </form>
-                </body>
-"""
-
 def generate_html(answers):
     html_answers = ""
     for answer in answers:
-        html_answers += f'<input type="radio" name="answer" value="{answer[0]}">{answer}<br>'
+        html_answers += f'<input type="radio" name="answer" value="{answer}">{answer}<br>'
     return html_answers
 
 questions = read_questions("questions.txt")
 
-class QuizHandler(BaseHTTPRequestHandler):
-    correct = None
-
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-
-        question = select_question(questions)
-        self.correct = question.correct_answer
-        
-        #answers = [(i, answer) for i, answer in enumerate(question.answers)]
-        answers_html = generate_html(question.answers)
-        html = html_template.format(question=question.question, answers=answers_html)
-        self.wfile.write(html.encode())
-
-    def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content_length).decode()
-        parsed_data = parse_qs(post_data)
-        user_answer = parsed_data['answer'][0]
-        
-        
-        if user_answer == self.correct:
-            response = "Correct!"
-        else:
-            response = "Incorrect!"
-        
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        html = html_response.format(response=response)
-        
-        self.wfile.write(html.encode())
-
-def main():
-    #questions = read_questions("questions.txt")
+class QuizHandler:
+    total = 0
+    correct = 0
+    previous_question = None
     
+    def handle_request(self, request):
+        if not request:
+            return self.handle_get('/')
 
-    for question in questions:
-        print("Question:", question.question)
-        print("Answers:", question.answers)
-        print("Correct answer:", question.correct_answer)
+        print("--------------------------")
+        print("Handle Request")
+        print(request)
+        print("--------------------------")
+
+        method, path, headers, body = self.parse_request(request)
+
+        if(path.find('?') != -1):
+            path = path[:path.find('?')]
         
-    try :
-        server = HTTPServer(('localhost', 8080), QuizHandler)
-        print('Started server on http://localhost:8080')
-        server.serve_forever()
-    except KeyboardInterrupt:
-        server.shutdown()
-        server.server_close()
-        print('Server stopped')
+        print("--------------------------")
+        print("Method: ", method)
+        print("Path: ", path)
+        print("Headers: ", headers)
+        print("Body: ", body)
+        print("--------------------------")
 
-   
+
+        if method == 'GET':
+            return self.handle_get(path)
+        elif method == 'POST':
+            return self.handle_post(path, body)
+        else:
+            return self.not_allowed()
+
+    def parse_request(self, request):
+        if not request:
+            return None, None, None, None
+
+        lines = request.split('\r\n')
+        method, path, _ = lines[0].split(' ')
+        headers = {}
+        for line in lines[1:]:
+            if not line:
+                break
+            key, value = line.split(': ')
+            headers[key] = value
+        body = '\r\n'.join(lines[lines.index('') + 1:])
+        return method, path, headers, body
+
+    def handle_get(self, path):
+        if path == '/':
+            while True:
+                question = select_question(questions)
+                if question != QuizHandler.previous_question:
+                    QuizHandler.previous_question = question
+                    break
+            
+            html_answers = generate_html(question.answers)
+            html_response = f"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE html><html><head><title>COS 332 Assignment 4</title></head><body><h1>Quiz</h1><h2>{question.question}</h2><form method='POST'><input type='hidden' name='correct_answer' value='{question.correct_answer}'>{html_answers}<input type='submit' value='Submit'></form></body></html>"
+            return html_response.encode()
+        if path == '/endquiz':
+            print("Correct answers: ", QuizHandler.correct, " out of ", QuizHandler.total)
+            html_response =  f"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE html><html><head><title>COS 332 Assignment 4</title></head><body><h1>Quiz</h1><h2>Quiz ended</h2><h3>Correct answers: {QuizHandler.correct} out of {QuizHandler.total}</h3><form method='GET' action='/'><input type='submit' value='Start quiz'></form></body></html>"
+            html_response = html_response.encode()
+            QuizHandler.correct = 0
+            QuizHandler.total = 0
+            return html_response
+        else:
+            return self.not_found()
+
+    def handle_post(self, path, body):
+        if path == '/':
+            if not body:
+                return self.invalid_request()
+            
+            parsed_body = dict(x.split("=") for x in body.split("&"))
+
+            if 'answer' not in parsed_body or 'correct_answer' not in parsed_body:
+                return self.invalid_request()
+            
+            QuizHandler.total += 1
+            
+            user_answer = parsed_body['answer']
+            correct_answer = parsed_body['correct_answer']
+
+            if user_answer == correct_answer:
+                response = "Correct!"
+                QuizHandler.correct += 1
+            else:
+                correct_answer = unquote(correct_answer)
+                correct_answer = correct_answer.replace("+", " ")
+                response = "Incorrect! The correct answer is: " + correct_answer
+            html_response = f"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE html><html><head><title>COS 332 Assignment 4</title></head><body><h1>Quiz</h1><h2>{response}</h2><form method='GET' action='/'><input type='submit' value='Next Question'></form><form method='GET' action='/endquiz'><input type='submit' value='End quiz'></form></body></html>"
+            return html_response.encode()
+        else:
+            return self.not_found()
+
+    def not_allowed(self):
+        return b"HTTP/1.1 405 Method Not Allowed\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE html><html><head><title>405 Method Not Allowed</title></head><body><h1>405 Method Not Allowed</h1><a href='/'>Go to home</a></body></html>"
+
+    def not_found(self):
+        return b"HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE html><html><head><title>404 Not Found</title></head><body><h1>404 Not Found</h1><a href='/'>Go to home</a></body></html>"
+
+    def invalid_request(self):
+        return b"HTTP/1.1 400 Bad Request\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE html><html><head><title>400 Bad Request</title></head><body><h1>400 Bad Request</h1><a href='/'>Go to home</a></body></html>"
+    
+    
+def main():
+
+    port = 8080
+
+    quiz_handler = QuizHandler()
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('localhost', port))
+    server_socket.listen(1)
+    print('Started server on http://localhost:' + str(port))
+    while True:
+        client_socket, _ = server_socket.accept()
+        request = client_socket.recv(1024).decode()
+        response = quiz_handler.handle_request(request)
+        client_socket.sendall(response)
+        client_socket.close()
 
 if __name__ == "__main__":
     main()
