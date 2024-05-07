f55fb2f08e068535cfc32796d686479303c880bd
FuryOfD <dharshanng@gmail.com>
2024-05-07 22:27:59 +0200

    edits

--- checkEmail.py
+++ checkEmail.py
@@ -26,28 +26,44 @@ def checkingEmail(username, password):
         pop_socket_ssl.sendall(f'PASS {password}\r\n'.encode())
         print(pop_socket_ssl.recv(4096).decode())
 
-        # Send STAT command to get the total number of messages and the total size of the mailbox
-        pop_socket_ssl.sendall(b'STAT\r\n')
-        response = pop_socket_ssl.recv(4096).decode()
-        num_messages, _ = response.split()[1:]
+        # # Send STAT command to get the total number of messages and the total size of the mailbox
+        # pop_socket_ssl.sendall(b'STAT\r\n')
+        # response = pop_socket_ssl.recv(4096).decode()
+        # num_messages, _ = response.split()[1:]
 
-        # Fetch the most recent email
-        pop_socket_ssl.sendall(f'RETR {num_messages}\r\n'.encode())
-        email_data = b''
-        while True:
-            data = pop_socket_ssl.recv(4096)
-            if not data:
-                break
-            email_data += data
+        # # Fetch the most recent email
+        # pop_socket_ssl.sendall(f'RETR {num_messages}\r\n'.encode())
+        # email_data = b''
+        # while True:
+        #     data = pop_socket_ssl.recv(4096)
+        #     if not data:
+        #         break
+        #     email_data += data
 
-        # Parse the email
-        email_text = email_data.decode()
-        msg = Parser().parsestr(email_text)
+        # # Parse the email
+        # email_text = email_data.decode()
+        # msg = Parser().parsestr(email_text)
 
-        # Process the most recent email
-        print("Subject:", msg['Subject'])
-        print("From:", msg['From'])
-        print("To:", msg['To'])
+        # # Process the most recent email
+        # print("Subject:", msg['Subject'])
+        # print("From:", msg['From'])
+        # print("To:", msg['To'])
+        
+        # Fetch headers of the first 5 messages
+        for i in range(1, 3):
+            pop_socket_ssl.sendall(f'TOP {i} 0\r\n'.encode())  # Get only the headers
+            headers_data = b''
+            while True:
+                data = pop_socket_ssl.recv(4096)
+                if not data or b'\r\n\r\n' in data:  # End of headers
+                    break
+                headers_data += data
+            
+            # Parse and process the headers
+            headers_text = headers_data.decode()
+            msg = Parser().parsestr(headers_text)
+            print(f"Message {i} - Subject: {msg['Subject']}, From: {msg['From']}, To: {msg['To']}")
+
 
         # Close connection
         pop_socket_ssl.sendall(b'QUIT\r\n')
